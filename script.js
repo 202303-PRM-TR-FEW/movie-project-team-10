60bc2d966b58ba210a0e11bb490efd4008bf4734
Burcu Bayraö <burju7@yahoo.com>
2023-05-29 14:28:37 +0300

    single movie page done

--- script.js
+++ script.js
@@ -25,8 +25,10 @@ const movieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
   const movieCast = await fetchCast(movie.id);
   const relatedMovies = await fetchRelatedMovies(movie.id);
-  renderMovie(movieRes, movieCast, relatedMovies);
-  console.log(relatedMovies);
+  const director = await fetchDirector(movie.id);
+  const trailers = await fetchTrailers(movie.id);
+  renderMovie(movieRes, movieCast, relatedMovies, director, trailers);
+  // console.log(trailers);
 };
 
 // This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
@@ -48,8 +50,25 @@ const fetchRelatedMovies = async (movie_id) => {
   const url = constructUrl(`movie/${movie_id}/similar`);
   const res = await fetch(url);
   const data = await res.json();
-  // console.log(data.results)
+  // console.log(data.results);
   return data.results;
+};
+
+//function for fetching trailers
+const fetchTrailers = async (movie_id) => {
+  const url = constructUrl(`movie/${movie_id}/videos`);
+  const res = await fetch(url);
+  const data = await res.json();
+  // console.log(data.results);
+  return data;
+};
+
+//function for fetching director
+const fetchDirector = async (movie_id) => {
+  const url = constructUrl(`movie/${movie_id}/credits`);
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.crew.find((cast) => cast.job === "Director");
 };
 
 // You'll need to play with this function in order to add features and enhance the style.
@@ -77,8 +96,8 @@ const renderMovies = (movies) => {
       <div class="card-body">
         <h5 class="card-title text-center">${movie.title}</h5>
         <p class="card-text">${movie.overview.slice(0, 100)}</p>
-        <p class="card-text ">vote Average: ${movie.vote_average}</p>
-        <p class="card-text">vote Count: ${movie.vote_count}</p>    
+        <p class="card-text "><b>Vote Average:</b> ${movie.vote_average}</p>
+        <p class="card-text"><b>Vote Count:</b> ${movie.vote_count}</p>    
       </div>
     </div>`;
 
@@ -92,44 +111,68 @@ const renderMovies = (movies) => {
 };
 
 // You'll need to play with this function in order to add features and enhance the style.
-const renderMovie = (movie, movieCast, relatedMovies) => {
+const renderMovie = (movie, movieCast, relatedMovies, director, trailers) => {
   CONTAINER.innerHTML = `
     <div class="row">
-        <div class="col-md-4">
-             <img id="movie-backdrop" src=${
-               movie.backdrop_path == null
-                 ? "images/movieLogo2.jpg"
-                 : BACKDROP_BASE_URL + movie.backdrop_path
-             }>
-        </div>
-        <div class="col-md-8">
-            <h2 id="movie-title">${movie.title}</h2>
-            <p id="movie-release-date"><b>Release Date:</b> ${
-              movie.release_date
-            }</p>
-            <p id="movie-runtime"><b>Runtime:</b> ${movie.runtime} Minutes</p>
-            <p id="movie-language"><b>Language:</b> ${movie.spoken_languages.map(
-              (languge) => `${languge.english_name}`
-            )}</p>
+      <div class="col-md-4">
+        <img id="movie-backdrop" src=${
+          movie.backdrop_path == null
+            ? "images/movieLogo2.jpg"
+            : BACKDROP_BASE_URL + movie.backdrop_path
+        }>
+      </div>
+      <div class="col-md-8">
+        <h2 id="movie-title">${movie.title}</h2>
+        <p id="movie-release-date"><b>Release Date:</b> ${
+          movie.release_date
+        }</p>
+        <p id="movie-runtime"><b>Runtime:</b> ${movie.runtime} Minutes</p>
+        <p id="movie-language"><b>Language:</b> ${movie.spoken_languages.map(
+          (languge) => `${languge.english_name}`
+        )}</p>
+        <p id="director"><b>Director:</b> ${director.name}</p> 
+        <p id="voteAvarage"><b>Vote Average:</b> ${movie.vote_average}</p>
+        <p id="voteCount"><b>Vote Count:</b> ${movie.vote_count}</p>
+        <p id="movie-production"><b>Production:</b> ${movie.production_companies.map(
+          (company) =>
+            `<img id="production-img" src="${
+              company.logo_path == null
+                ? "images/movieLogo2.jpg"
+                : BACKDROP_BASE_URL + company.logo_path
+            }" alt="${company.name}" style="height:60px;"`
+        )}</p>      
+        <h3>Overview:</h3>
+        <p id="movie-overview">${movie.overview}</p>     
+      </div>
+    </div>
 
-            <h3>Overview:</h3>
-            <p id="movie-overview">${movie.overview}</p>
-        </div>
-        </div>
-            <h3>Actors:</h3>
-            <ul id="actors" class="list-unstyled list-group list-group-horizontal"></ul>
-        
-            <h3>Related Movies:</h3>
-            <ul id="relatedMoviesList" class="list-unstyled list-group list-group-horizontal"></ul>
+    <div class="row">
+      <h3>Trailer:</h3>
+      <div id="movie-trailer">
+        <iframe class="trailer" src="https://www.youtube.com/embed/${
+          trailers.results[0].key
+        }?autoplay=1" style="height:500px;width:900px;"></iframe>
+      </div>
+    </div> 
 
+    <div class="row">
+      <h3>Actors:</h3>
+      <ul id="actors" class="list-unstyled list-group list-group-horizontal"></ul>
+    </div>
+
+    <div class="row">
+      <h3>Related Movies:</h3>
+      <ul id="relatedMoviesList" class="list-unstyled list-group list-group-horizontal"></ul>
     </div>`;
+  // console.log(movieCast);
+
   renderCast(movieCast);
   renderRelatedMovies(relatedMovies);
 };
 
 //function for related movies
 const renderRelatedMovies = (relatedMovies) => {
-  console.log(relatedMovies);
+  // console.log(relatedMovies);
   const relatedMoviesList = document.querySelector("#relatedMoviesList");
   relatedMovies.slice(0, 5).map((movie) => {
     const movieCard = document.createElement("li");
@@ -329,17 +372,16 @@ const renderCast = (movieCast) => {
 
 document.addEventListener("DOMContentLoaded", autorun);
 
-
 // About Page /w inner HTML
 
 const aboutPage = () => {
-  CONTAINER.innerHTML=`
+  CONTAINER.innerHTML = `
   <div class="about-container">
     <p class="about-text">" When We wrote this code, only god and we knew how it worked. Now, only god knows it! "</p>
     <p class="about-text2"> - Anonymous</p>
     <p class= "imageContainer"><img class="aboutImage" src="images/dogecoin-2-1-1138x640.jpg"></p>
-  </div>`
-}
+  </div>`;
+};
 
 const aboutBtn = document.querySelector("#aboutBtn");
 aboutBtn.addEventListener("click", () => {
@@ -347,102 +389,93 @@ aboutBtn.addEventListener("click", () => {
   aboutPage();
 });
 
+/// filter and genre dropdown section
 
-/// filter and genre dropdown section 
-
-
-
-// fetching genre and adding element to the movie dropdown 
-const dropDownContent = document.getElementById("dropdown-movie")
+// fetching genre and adding element to the movie dropdown
+const dropDownContent = document.getElementById("dropdown-movie");
 const url = constructUrl("genre/movie/list");
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    data.genres.forEach(genre => {
-      const options = document.createElement('li')
-      dropDownContent.appendChild(options)
+  .then((response) => response.json())
+  .then((data) => {
+    data.genres.forEach((genre) => {
+      const options = document.createElement("li");
+      dropDownContent.appendChild(options);
 
-      const option = document.createElement('a')
-      options.appendChild(option)
-      option.classList.add('dropdown-item')
-      option.value = genre.id
-      option.href = '#'
-      option.textContent = genre.name
-      
-    option.addEventListener('click', function(event) {
-      const genreId = genre.id
-      genreIdfetcher(genreId)
-    })
-  })
-})
+      const option = document.createElement("a");
+      options.appendChild(option);
+      option.classList.add("dropdown-item");
+      option.value = genre.id;
+      option.href = "#";
+      option.textContent = genre.name;
 
-// Handeler of fetching the genre based on the selected id 
+      option.addEventListener("click", function (event) {
+        const genreId = genre.id;
+        genreIdfetcher(genreId);
+      });
+    });
+  });
+
+// Handeler of fetching the genre based on the selected id
 function genreIdfetcher(id) {
-    fetch(`${TMDB_BASE_URL}/discover/movie?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0&with_genres=${id}`)
-  .then(resp => resp.json())
-  .then(data => {
-    renderMovies(data.results)
-  })
+  fetch(
+    `${TMDB_BASE_URL}/discover/movie?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0&with_genres=${id}`
+  )
+    .then((resp) => resp.json())
+    .then((data) => {
+      renderMovies(data.results);
+    });
 }
-
 
 // Handeling and ftching the dropdown filter
 
-function fetchingAndAssigninFilterDropDown () {
-
-// Filtering popularity
-  const dropdownPopular = document.getElementById("popular-drop-item")
+function fetchingAndAssigninFilterDropDown() {
+  // Filtering popularity
+  const dropdownPopular = document.getElementById("popular-drop-item");
   dropdownPopular.addEventListener("click", () => {
-  fetch(`${TMDB_BASE_URL}/movie/popular?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0`)
-  .then(resp => resp.json())
-  .then(data => {
-    renderMovies(data.results)
-    })
-  })
+    fetch(
+      `${TMDB_BASE_URL}/movie/popular?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        renderMovies(data.results);
+      });
+  });
 
-// Filtering top rated movies
-  const dropdowntopRated = document.getElementById("topRated-drop-item")
-  dropdowntopRated.addEventListener('click', () => {
-  fetch(`${TMDB_BASE_URL}/movie/top_rated?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0`)
-  .then(resp => resp.json())
-  .then(mata => {
-    renderMovies(mata.results)
-    })
-  })
+  // Filtering top rated movies
+  const dropdowntopRated = document.getElementById("topRated-drop-item");
+  dropdowntopRated.addEventListener("click", () => {
+    fetch(
+      `${TMDB_BASE_URL}/movie/top_rated?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0`
+    )
+      .then((resp) => resp.json())
+      .then((mata) => {
+        renderMovies(mata.results);
+      });
+  });
 
-// Filtering Now playing movies
-  const dropdownNowPlaying = document.getElementById("nowPlaying-drop-item")
-  dropdownNowPlaying.addEventListener('click', () => {
-  fetch(`${TMDB_BASE_URL}/movie/now_playing?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0`)
-  .then(resp => resp.json())
-  .then(data => {
-    renderMovies(data.results)
-    })
-  })
+  // Filtering Now playing movies
+  const dropdownNowPlaying = document.getElementById("nowPlaying-drop-item");
+  dropdownNowPlaying.addEventListener("click", () => {
+    fetch(
+      `${TMDB_BASE_URL}/movie/now_playing?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        renderMovies(data.results);
+      });
+  });
 
-// Filtering upcoming movies
-  const dropdownUpComing = document.getElementById("upComing-drop-item")
-  dropdownUpComing.addEventListener('click', () => {
-  fetch(`${TMDB_BASE_URL}/movie/upcoming?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0`)
-  .then(resp => resp.json())
-  .then(data => {
-    renderMovies(data.results)
-    })
-  })
-  
+  // Filtering upcoming movies
+  const dropdownUpComing = document.getElementById("upComing-drop-item");
+  dropdownUpComing.addEventListener("click", () => {
+    fetch(
+      `${TMDB_BASE_URL}/movie/upcoming?api_key=7bdd4afb1f1beea5c5d1ed26587d9ea0`
+    )
+      .then((resp) => resp.json())
+      .then((data) => {
+        renderMovies(data.results);
+      });
+  });
 }
 
-fetchingAndAssigninFilterDropDown()
-
-
-
-
-
-
-
-
-
-
-
-
-
+fetchingAndAssigninFilterDropDown();
